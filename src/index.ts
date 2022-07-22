07bd232b1529b0cd82b331b526f852f512a9db94
Nick G <nicholasgalante1997@gmail.com>
2022-07-22 12:11:16 -0600

    [asana-3] convert http server module to use combination express/https.createServer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import http, { IncomingMessage, ServerResponse } from 'http';
+import http from 'http';
 import dotenv from 'dotenv';
-import url from 'url';
+import express, { Request, Response, NextFunction } from 'express';
 import { log } from '@nickgdev/couch-gag-common-lib';
 import { router } from './router';
 import { getOptionsResponse, cors } from './cors';
@@ -10,42 +10,57 @@ dotenv.config();
 
 const PORT = process.env.PORT || (2023 as const);
 
-const server = http.createServer(function (
-  req: IncomingMessage,
-  res: ServerResponse
-) {
-  const { url: reqUrl, headers } = req;
-  const verified: boolean =
-    headers['x-ulysses-key'] === process.env.ULYSSES_HASHED_KEY;
+const app = express();
 
-  cors(res);
-
-  if (req.method === 'OPTIONS') {
-    getOptionsResponse(res);
-    return;
-  }
-
-  if (!reqUrl) {
-    handleInvalidUrl(res);
-    return;
-  }
-
-  if (!verified) {
+function verifyUlyssesKey(req: Request, res: Response, next: NextFunction){
+  const { headers } = req;
+  const verified: boolean = headers['x-ulysses-key'] === process.env.ULYSSES_HASHED_KEY;
+  if (verified) {
+    next();
+  } else {
     handleInvalidUlyssesKey(res);
     return;
   }
+}
 
-  if (verified && reqUrl) {
-    const parsed = url.parse(reqUrl!, true);
-    let { pathname } = parsed;
-    if (pathname !== '/all' && pathname !== '/markdown') {
-      pathname = 'default';
-    }
-    const handle = router[pathname as keyof typeof router];
-    handle(req, res);
+/** locally defined cors */
+app.use((_req: Request, res: Response, next: NextFunction) => {
+  cors(res);
+  next();
+})
+
+/** handle options prefetch */
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  if (req.method === 'OPTIONS') {
+    getOptionsResponse(res);
     return;
+  } else {
+    next();
   }
-});
+})
+
+/** handle invalid url on request */
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const { url: reqUrl, headers } = req;
+  if (!reqUrl) {
+    handleInvalidUrl(res);
+    return;
+  } else {
+    next();
+  }
+})
+
+/** verify ulysses key */
+app.use(verifyUlyssesKey);
+
+app.get('/all', router['/all']);
+
+app.get('/markdown', router['/markdown']);
+
+app.use('*', router.default);
+
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
   log('info', 'Server started on ' + PORT);
